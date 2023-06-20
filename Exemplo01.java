67b8e8580c2be7954f88cece0d61b1af9dd9e71a
pabllo guilherme <pablloguilhermecandido@gmail.com>
2023-06-19 22:08:32 -0400

    primeiro comite

---
+++ Exemplo01.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjetoGoltz;
+
+/**
+ *
+ * @author cocco
+ */
+public class Exemplo01 {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!! ");
+        System.out.println("Aprendendo git!!");
+    }
+    
+}
